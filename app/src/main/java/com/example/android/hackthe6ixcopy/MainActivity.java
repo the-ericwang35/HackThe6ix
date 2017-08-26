6eaabf15c03d73bfb2dd66c0495ba9251c22f384
Jerry Li <jerry@jerryli.ca>
2017-08-26 19:33:21 -0400

    add counter

--- app/src/main/java/com/example/android/hackthe6ixcopy/MainActivity.java
+++ app/src/main/java/com/example/android/hackthe6ixcopy/MainActivity.java
@@ -87,12 +87,8 @@ public class MainActivity extends AppCompatActivity {
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (done) {
-                    done = false;
+                if (picNumber == 0) {
                     takePicture();
-                }
-                else {
-                    done = true;
                 }
             }
         });
@@ -143,11 +139,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
             super.onCaptureCompleted(session, request, result);
-            if (!done){
+            if (picNumber < 11){
                 takePicture();
             }
             else {
-                Toast.makeText(MainActivity.this, "Saved:" + file, Toast.LENGTH_SHORT).show();
+                picNumber = 0;
+                Toast.makeText(MainActivity.this, "Saved: " + picNumber + " Files", Toast.LENGTH_SHORT).show();
                 createCameraPreview();
             }
         }
@@ -238,7 +235,6 @@ public class MainActivity extends AppCompatActivity {
                 public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
                     super.onCaptureCompleted(session, request, result);
                     if (!done){
-                        updatePreview();
                         takePicture();
                     }
                     else {
