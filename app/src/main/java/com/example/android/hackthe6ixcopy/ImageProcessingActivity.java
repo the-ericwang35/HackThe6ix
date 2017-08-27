a28422ce5403a4293d7e887ab4fb91e227ea370d
Tony Liao <tony.liao.5966@gmail.com>
2017-08-27 00:24:55 -0400

    Add intent to start new activity

--- app/src/main/java/com/example/android/hackthe6ixcopy/ImageProcessingActivity.java
+++ app/src/main/java/com/example/android/hackthe6ixcopy/ImageProcessingActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.hackthe6ixcopy;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class ImageProcessingActivity extends AppCompatActivity {
 
@@ -15,8 +19,11 @@ public class ImageProcessingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_processing);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        File folder = new File(Environment.getExternalStorageDirectory() + "/pics/");
+        File output = new File(Environment.getExternalStorageDirectory() + "/pics/median.jpg");
+        MedianCalculator.process(Arrays.asList(folder.listFiles()), output);
     }
 
 }
