d1b637d2e2164f4fd579630b37b55ac5b1e63f3b
the-ericwang35 <the.ericwang35@gmail.com>
2017-08-27 02:09:14 -0400

    Call async task from image processing activity

--- app/src/main/java/com/example/android/hackthe6ixcopy/ImageProcessingActivity.java
+++ app/src/main/java/com/example/android/hackthe6ixcopy/ImageProcessingActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.hackthe6ixcopy;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -19,9 +20,26 @@ public class ImageProcessingActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        
-        TextView text = (TextView) findViewById(R.id.text);
-        text.setText("done processing");
+        new ProcessImagesTask().execute(0); //0 is a placeholder
     }
 
+    class ProcessImagesTask extends AsyncTask<Integer, Void, Void> {
+        @Override
+        protected Void doInBackground(Integer...params){
+            File folder = new File(Environment.getExternalStorageDirectory() + "/pics/");
+            File output = new File(Environment.getExternalStorageDirectory() + "/pics/median.jpg");
+            ThingRemover.process(Arrays.asList(folder.listFiles()), output);
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            TextView text = (TextView) findViewById(R.id.text);
+            text.setText("done processing");
+        }
+    }
 }
